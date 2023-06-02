22c5e47946e43dfd363344c8d836e4523d90a082
Frank Florian <fflorianc@gmail.com>
2023-06-02 15:51:44 -0500

    1

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,14 +1,15 @@
-const { Router } = require('express');
-const { createUser, getUserToDoCategory } = require('../controllers/users.controllers');
+const { Router } = require("express");
+const {
+  createUser,
+  getUserToDoCategory,
+} = require("../controllers/users.controllers");
 
 const router = Router();
 
 // createUser
-router.post('/users', createUser);
+router.post("/", createUser);
 
-//getUserToDoCategory
-router.get('/users/:id', getUserToDoCategory)
+// getUserToDoCategory
+router.get("/:id", getUserToDoCategory);
 
 module.exports = router;
-
-
